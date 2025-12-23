2cf0b396d9cb21d2534f927168ea04666df058f6
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-12-22 23:46:08 -0300

    ok

--- chat-gepeteco/src/app/core/chat/msg/msg.component.ts
+++ chat-gepeteco/src/app/core/chat/msg/msg.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Message } from '../../../models/chat.model';
 import { CommonModule } from '@angular/common';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-msg',
@@ -10,4 +11,21 @@ import { CommonModule } from '@angular/common';
 })
 export class MsgComponent {
     @Input() message!: Message;
+    formattedContent: SafeHtml = '';
+    
+    constructor(private sanitizer: DomSanitizer) {}
+    
+    ngOnChanges() {
+        if (this.message && this.message.text) {
+            this.formattedContent = this.formatText(this.message.text);
+        }
+    }
+    
+    formatText(text: string): SafeHtml {
+        let formatted = text;
+        formatted = formatted.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+        formatted = formatted.replace(/^\s*\*\s+(.*)$/gm, '<div class="ml-4">• $1</div>');
+        formatted = formatted.replace(/\n/g, '<br>');
+        return this.sanitizer.bypassSecurityTrustHtml(formatted);
+    }
 }
